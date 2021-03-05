9a490485a66a3aaa8deaea099e3b075c7cef98dc
Jeanettable <jeanetteabell@gmail.com>
2021-03-05 12:27:05 -0800

    ls working

--- bash.js
+++ bash.js
@@ -4,6 +4,8 @@ process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
   if (cmd === "pwd") {
     require("./pwd.js");
+  } else if(cmd === "ls") {
+    require("./ls");
   } else {
     process.stdout.write("You typed: " + cmd);
   }
